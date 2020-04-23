058e36e9267f737bb65ca4a1c702709351fbbde6
Jianjin Xu <jx2386@columbia.edu>
2020-04-23 22:54:42 +0800

    Add VSCode plugin

---
+++ Mobile GamAR/Assets/Scripts/LoadTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTexture : MonoBehaviour
+{
+    public GameObject EIT;
+    private GameObject imageTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
