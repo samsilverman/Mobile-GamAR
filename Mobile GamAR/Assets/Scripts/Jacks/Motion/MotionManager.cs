219a5b2a6dd37a3c508f849ee808ec934df0fdbf
Samuel Silverman <mr.samsilverman@gmail.com>
2020-05-07 21:07:19 -0400

    Add movement to Jacks world

---
+++ Mobile GamAR/Assets/Scripts/Jacks/Motion/MotionManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionManager : MonoBehaviour
+{
+    public Transform world;
+
+    public float speed;
+
+    bool movingLeft;
+    bool movingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StopMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingLeft)
+        {
+            world.Rotate(0f, speed * Time.deltaTime, 0f);
+        }
+
+        else if (movingRight)
+        {
+            world.Rotate(0f, -speed * Time.deltaTime, 0f);
+        }
+    }
+
+    public void MoveLeft()
+    {
+        movingLeft = true;
+    }
+
+    public void MoveRight()
+    {
+        movingRight = true;
+    }
+
+    public void StopMove()
+    {
+        movingLeft = false;
+        movingRight = false;
+    }
+
+}
