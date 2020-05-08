39a0a16460767e6db45f15e46c0ba2f26f644747
Samuel Silverman <mr.samsilverman@gmail.com>
2020-05-08 14:15:56 -0400

    Connect scenes together in main menu and home buttons

---
+++ Mobile GamAR/Assets/Scripts/Scenes/SceneLoadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void LoadPlayingCardsScene()
+    {
+        SceneManager.LoadScene("PlayingCardsScene");
+    }
+
+    public void LoadJacksScene()
+    {
+        SceneManager.LoadScene("JacksScene");
+    }
+}
